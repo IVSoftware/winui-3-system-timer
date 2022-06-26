f51dea098ad1250c7d7b3f85b5be49ad5967c3a6
Tom Gregor <tgregor@ivsoftware.com>
2022-06-26 06:39:44 -0600

    Add project files.

---
+++ winui_3_system_timer/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using System;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winui_3_system_timer
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            // https://stackoverflow.com/a/71074431/5438626
+            IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this); // m_window in App.cs
+            WindowId windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+
+            var size = new Windows.Graphics.SizeInt32();
+            size.Width = 480;
+            size.Height = 800;
+
+            appWindow.Resize(size);
+            var dispatcherQueue = Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread();
+
+            SystemTimer.PropertyChanged += (sender, e) =>
+            {
+                switch (e.PropertyName)
+                {
+                    case nameof(SystemTimer.Second):
+                        dispatcherQueue.TryEnqueue(() =>
+                        {
+                            textboxClock.Text = SystemTimer.Second.ToString("hh:mm:ss");
+                        });
+                        break;
+                }
+            };
+        }
+    }
+}
---
+++ winui_3_system_timer/SystemTimer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace winui_3_system_timer
+{
+    static class SystemTimer
+    {
+        static SystemTimer()
+        {
+            Task.Run(async () =>
+            {
+                while (!_dispose)
+                {
+                    PushDT(DateTime.Now);
+                    await Task.Delay(100);
+                }
+            });
+        }
+
+        public static void PushDT(DateTime now)
+        {
+            // Using a 'now' that doesn't change within this method:
+            Second = new DateTime(now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second, now.Kind);
+        }
+        public static event PropertyChangedEventHandler PropertyChanged;
+        static DateTime _second = DateTime.MinValue;
+        public static DateTime Second
+        {
+            get => _second;
+            set
+            {
+                if (_second != value)
+                {
+                    _second = value;
+                    PropertyChanged?.Invoke(nameof(SystemTimer), new PropertyChangedEventArgs(nameof(Second)));
+                }
+            }
+        }
+        static bool _dispose = false;
+    }
+}
